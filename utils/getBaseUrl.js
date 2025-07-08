bb56cff06a243de2ab3b7e93f8b2e9f9c869cbd7
komal ,k c~x <komaalfarid@gmail.com>
2025-07-08 11:09:19 +0500

    changes in code

--- utils/getBaseUrl.js
+++ utils/getBaseUrl.js
@@ -1,8 +1,4 @@
 export function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    return window.location.origin; // On client
-  }
-
   // On server
   return process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 }
